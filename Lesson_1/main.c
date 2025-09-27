2d3a2f28b0cfc8317201d718adcea7dc72eb78ab
= <=>
2025-09-27 10:32:34 -0400

    taks slove1

--- Lesson_1/main.c
+++ Lesson_1/main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
+#include <limits.h>
 
 int main(void){
-    char name[50];
-    printf("Hello! What is your name: ");
-    scanf("%s", name);
-    printf("Hello %s!", name);
+    int n,q, reverse = 0;
+    scanf("%d", &n);
+    int arr[n];
+
+    int max_val= INT_MIN;
+    int min_val = INT_MAX;
+    int max_val_id=0, min_val_id=0;
+
+    
+    for(int i=0; i<n; i++){
+        scanf("%d", &q);
+
+        if (q > max_val) { max_val = q; max_val_id = i; }
+        if (q < min_val) { min_val = q; min_val_id = i; }
+        
+        arr[i] = q;
+    }
+
+    if(min_val_id > max_val_id) reverse = 1;  // если максимальное чило левее минимального
+
+    for(int i=0; i<n; i++){
+        if(i == min_val_id || i == max_val_id){
+            if(reverse){
+                for(int k=min_val_id; k>=max_val_id; k--) printf("%d ", arr[k]);
+                i = min_val_id;
+            }
+            else{
+                for(int k=max_val_id; k>=min_val_id; k--) printf("%d ", arr[k]);
+                i = max_val_id;
+            }
+        }
+        else printf("%d ", arr[i]);
+    }
 }
